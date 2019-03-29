e3e2bf1d1363df15771c704f7c8d67317ce0887b
unknown <hampton.muriel@gmail.com>
2019-03-29 14:04:12 -0400

    undescriptive push

--- liri.js
+++ liri.js
@@ -2,16 +2,18 @@
 var axios = require('axios');
 var dotenv = require("dotenv").config(); // is this right 
 var moment = require('moment');
-require("dotenv").config(); // or is this right? 
-// var keys = require("./keys.js");
-// var spotify = new Spotify(keys.spotify);
+
+var keys = require("./keys.js");
+var Spotify = require('node-spotify-api'); //Spotify (package) vs spotify(importing keys) **
+var spotify = new Spotify(keys.spotify); //might need to inside spotify()
+
 
 //don't forget to do the rest of the requires
 var action = process.argv[2];
 //maybe a forEach loop to iterate through the remainder like process.argv[i]
-
-
 var input = process.argv[3];
+
+console.log(process.env.SPOTIFY_ID);
 
 //if time allows update this to a switch state
 if ((action === "movie-this") && (!input)) {
@@ -26,9 +28,9 @@ if ((action === "movie-this") && (!input)) {
 } else if (action === "concert-this") {
   band();
 } else if (action === "spotify-this-song") {
-  spotify();
+  spotifyFunction();
 } else {
-  console.log("Nope, don't know what that means! Try again");
+  console.log("Either you typed in a command I don't understand or you didn't type ib a command!");
   console.log("Commands: movie-this, concert-this, & spotify-this-song");
 }
 
@@ -45,14 +47,65 @@ function omdb(movie) {
         response.data.Country,
         response.data.Plot,
         response.data.Actors);
-       
     })
-    
   // .catch(function (error) {
   // console.log(error);
   // });
 }
 
+
+
+
+function band() {
+  // console.log("band is working");
+  // console.log('venue, venue location, eventDate');
+  axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp")
+    .then(function (response) {
+      for (i = 0; i <= 9; i++) { //not looping through just getting the first one. 
+        var convertedDate = moment(response.data[i].datetime).format('MMMM Do YYYY, h:mm:ss a');
+        // console.log("band() working");        
+        line();
+        console.log("Band Name: " + input);
+        console.log("Date of Concert: " + convertedDate);
+        console.log("Venue Name: " + response.data[i].venue.name);
+        console.log("Venue City: " + response.data[i].venue.city);
+      }
+    })
+    .catch(function (error) {
+      // console.log(error);
+    });
+}
+
+function spotifyFunction() {
+  console.log("spotify-this-song")
+  // console.log('artist, songName, previewlink, album');
+  // this is a promise you have to have the object defined. .. why no work
+  spotify.search({
+      type: 'track',
+      query: input,
+      limit: 5
+    })
+    .then(function (response) {
+
+      // console.log(response.tracks.items[0].name);
+      allTracks = response.tracks.items;
+      // console.log(allTracks);
+      allTracks.forEach(function(thisTrack){
+        console.log(thisTrack.name);
+      })
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+//I got real tired of typing console.log('-------') a bunch! lazy makes you better a coder :) 
+function line() {
+  console.log("-----------------");
+}
+
+
+//TRASHCAN
 // function bandArtist(){
 //constructor here????
 // }
@@ -65,38 +118,3 @@ function omdb(movie) {
 //   this.age = age;
 //   this.eyeColor = eye;
 // }
-
-
-function band() {
-  // console.log("band is working");
-  // console.log('venue, venue location, eventDate');
-  axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp")
-    .then(function (response) {      
-      for (i = 0; i <= 9; i++) { //not looping through just getting the first one. 
-        var convertedDate = moment(response.data[i].datetime).format('MMMM Do YYYY, h:mm:ss a');
-        // console.log("band() working");        
-        line();
-        console.log("Band Name: " + input);
-        console.log("Date of Concert: " + convertedDate);
-        console.log("Venue Name: " + response.data[i].venue.name);
-        console.log("Venue City: " + response.data[i].venue.city);
-        
-        // moment().format(console.log(response.data[1].datetime));
-        // console.log(response.data);
-        // console.log(JSON.stringify(response, null, 2)); I tried
-      }
-
-    })
-    .catch(function (error) {
-      // console.log(error);
-    });
-}
-
-function spotify() {
-  console.log("spotify-this-song")
-  // console.log('artist, songName, previewlink, album');
-}
-
-function line() {
-  console.log("-----------------");
-}
