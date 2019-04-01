028f34b6a2ea077d0483214e3d1a0e6cf079367b
unknown <hampton.muriel@gmail.com>
2019-04-01 17:13:59 -0400

    it livesgit add .!

--- keys.js
+++ keys.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require("dotenv").config(); // or is this right? 
 // console.log(process.env.SPOTIFY_ID);
 
@@ -8,14 +7,3 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
-=======
-require("dotenv").config(); // or is this right? 
-// console.log(process.env.SPOTIFY_ID);
-
-
-//in instructions
-exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
-};
->>>>>>> b1237e14fcb34c799db378cc8115e442ca92e69b
